5e3dceb7f2291ce5a987760fd6bdd1b547d0e73e
Ian C <collicottir@gmail.com>
2021-12-02 17:49:08 -0800

    add reverse integer solution

--- src/main/java/Problems.java
+++ src/main/java/Problems.java
@@ -5,6 +5,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Problems {
+    /**
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     *
+     * 4. Median of Two Sorted Arrays - Hard
+     *  Given two sorted arrays nums0 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+     *  The overall run time complexity should be O(log (m+n)).
+     */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         List<Integer> set = new ArrayList<Integer>();
         set.addAll(IntStream.of(nums1).boxed().collect(Collectors.toList()));
@@ -14,12 +24,44 @@ public class Problems {
         Arrays.sort(numArray);
         double median;
 
-        System.out.println(Arrays.toString(numArray));
         if (numArray.length % 2 == 0)
             median = ((double)numArray[numArray.length/2] + (double)numArray[numArray.length/2 - 1])/2;
         else
             median = (double) numArray[numArray.length/2];
 
         return median;
+    }
+
+    /**
+     * 7. Reverse Integer - Medium
+     * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go
+     *  outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+     * @param x
+     * @return
+     */
+    public static int reverseInt(int x) {
+        String s = String.valueOf(x);
+        int start = s.startsWith("-") ? 1 : 0;
+        byte[] input = s.getBytes();
+        byte[] arr = new byte[input.length];
+        int idxOffset = start > 0 ? input.length : input.length - 1;
+
+        for (int i = start; i < input.length; i++) {
+            arr[i] = input[idxOffset - i];
+        }
+
+        String out = new String(arr);
+        int outInt;
+
+        try {
+            outInt = Integer.parseInt(out.trim());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+
+        if (start > 0)
+            outInt *= -1;
+
+        return outInt;
     }
 }
--- src/test/java/ProblemsTest.java
+++ src/test/java/ProblemsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProblemsTest {
-
     @Test
     @DisplayName("4. Median of Two Sorted Arrays")
     void findMedianSortedArrays() {
@@ -13,6 +12,17 @@ class ProblemsTest {
                 () -> assertEquals(0, Problems.findMedianSortedArrays(new int[] {0, 0}, new int[] {0, 0})),
                 () -> assertEquals(1.0, Problems.findMedianSortedArrays(new int[] {}, new int[] {1})),
                 () -> assertEquals(2.0, Problems.findMedianSortedArrays(new int[] {2}, new int[] {}))
+        );
+    }
+
+    @Test
+    @DisplayName("7. Reverse Integer")
+    void reverseInt() {
+        assertAll(
+                () -> assertEquals(321, Problems.reverseInt(123)),
+                () -> assertEquals(-321, Problems.reverseInt(-123)),
+                () -> assertEquals(21, Problems.reverseInt(120)),
+                () -> assertEquals(0, Problems.reverseInt(0))
         );
     }
 }
